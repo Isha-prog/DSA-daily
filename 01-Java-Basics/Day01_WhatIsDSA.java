f630b10619b8c96c1b4183a19045f971871e2ac1
Isha-prog <rekhas.singh2004@gmail.com+>
2026-02-06 11:03:00 +0530

    Day 01 — Java Basics — What is DSA

---
+++ 01-Java-Basics/Day01_WhatIsDSA.java
@@ -0,0 +1,11 @@
+/*
+DSA = Data Structures + Algorithms
+Data Structure = how data is stored
+Algorithm = steps to solve problem
+*/
+
+public class Day01_WhatIsDSA {
+    public static void main(String[] args) {
+        System.out.println("Starting DSA Journey in Java");
+    }
+}
