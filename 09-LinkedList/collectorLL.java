3a70eaab02be7d51fa746b32a7c17a3492277197
Isha-prog <rekhas.singh2004@gmail.com>
2026-04-05 23:48:51 +0530

    used linkedlist of collection framework

---
+++ 09-LinkedList/collectorLL.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class collectorLL {
+    public static void main(String[] args) {
+        LinkedList<String> list=new LinkedList<>();
+        list.addFirst("isha");
+        list.addFirst("hii");
+        list.addFirst("oh");
+        System.out.println(list);
+        list.addLast("nice");
+        System.out.println(list);
+        System.out.println(list.size());
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+"->");
+        }
+        System.err.println("null");
+        list.removeFirst();
+        System.out.println(list);
+        list.removeLast();
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+    }
+}
