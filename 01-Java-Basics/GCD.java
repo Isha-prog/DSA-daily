bbb93e555f1a1fa875a93098182af7f8daf45572
Isha-prog <rekhas.singh2004@gmail.com>
2026-07-22 00:31:29 +0530

    Greatest Common Divisor of two numbers

---
+++ 01-Java-Basics/GCD.java
@@ -0,0 +1,16 @@
+public class GCD {
+  
+    static int gcd(int n1,int n2){
+      int gcd=1;
+      for(int i=1;i<=Math.min(n1,n2);i++){
+        if(n1%i==0 && n2%i==0){
+          gcd=i;
+        }
+      }
+      return gcd;
+    }
+   public static void main(String args[]){
+     System.out.println(gcd(12,18));
+   }
+
+}
